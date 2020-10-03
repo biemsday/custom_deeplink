4f84ecc47766d2cd1eb83ac5ef11a21416e6649c
Mikhail Basov <mbasov@ozon.ru>
2020-10-03 17:23:31 +0300

    add new user

--- app.js
+++ app.js
@@ -7,4 +7,12 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'))
 });
 
+app.get('/basova_yana', function(req, res) {
+    res.sendFile(path.join(__dirname + '/basova_yana/index.html'))
+});
+
+app.get('/svet_dyak', function(req, res) {
+    res.sendFile(path.join(__dirname + '/svet_dyak/index.html'))
+});
+
 app.listen(port, () => console.log(`istening on port ${port}!`));
